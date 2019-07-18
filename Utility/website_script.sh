cb846d1c9eb4f7707c20d61c068b1500c1d21af7
Yuan Cao <yuan.cao.email@gmail.com>
2019-07-17 22:41:57 -0400

    Add tool

---
+++ Utility/website_script.sh
@@ -0,0 +1,5 @@
+# Example: python -m webbrowser -t "http://www.python.org"
+
+python -m webbrowser -t "http://e.ggtimer.com/25%20minutes"
+python -m webbrowser -t "http://e.ggtimer.com/5%20minutes"
+
